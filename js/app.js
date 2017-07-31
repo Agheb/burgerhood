c4f994c03ca7c82be2a7cefc3f503b88c1c93458
Aman Ghebreweldi <amanuelg@me.com>
2017-07-31 04:52:29 +0200

    some boilerplate for webpack

--- js/app.js
+++ js/app.js
@@ -1,32 +1,40 @@
-import '../css/app.scss';
+import "../css/app.scss";
 import $ from "jquery";
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import 'waypoints';
-import 'scrollTo';
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "waypoints";
+import "scrollTo";
+import {request} from "graphql-request";
 
-/**
- * EXAMPLE JS STARTS
- */
-$(function() {
-    $('[id^=scrollTo]').click(function() {
-        var id = $(this).attr('id').slice(9);
-        $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
-    });
+const query = `{
+  Movie(title: "Inception") {
+    releaseDate
+    actors {
+      name
+    }
+  }
+}`
+  
+request('https://api.graph.cool/simple/v1/movies', query).then(data => console.log(data))
 
-    $('#marketing').waypoint(function() {
-        $('.img-circle').addClass('animated zoomIn');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
-
-    $('.featurette').waypoint(function() {
-        $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  if (document.querySelectorAll("#map").length > 0) {
+    var js_file = document.createElement("script");
+    js_file.type = "text/javascript";
+    js_file.src =
+      "https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyBeAR2QSBsI-Xch0uTh1yOADUDHifjzWTY";
+    document.getElementsByTagName("head")[0].appendChild(js_file);
+  }
 });
-/**
- * EXAMPLE JS ENDS
- */
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 8
+  });
+}
+window.initMap = initMap; //global scope 
