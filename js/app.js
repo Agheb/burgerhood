b52efa1c3b0c647144539f3951ae48e425035b3d
Aman Ghebreweldi <amanuelg@me.com>
2017-08-03 23:50:35 +0200

    set markers in Map

--- js/app.js
+++ js/app.js
@@ -1,25 +1,10 @@
 import "../css/app.scss";
 import $ from "jquery";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
-import {query} from "./gql.js"
-var ko = require('knockout');
-
-var LocationModel = require('../locations.json');
-
-LocationModel.Query = ko.observable('');
-
-LocationModel.searchResults = ko.computed(function() {
-    var q = LocationModel.Query().toLowerCase();
-    return LocationModel.locations.filter(function(i) {
-      return i.name.toLowerCase().indexOf(q) >= 0;
-    });
-});
-
-
-ko.applyBindings(LocationModel, document.getElementById('root'));
-
-
-
+import { query } from "./gql.js";
+var ko = require("knockout");
+var LocationModel = require("../locations.json");
+var map;
 /*
 const gql_query = `{ business(id: "yelp-san-francisco") 
             { 
@@ -31,21 +16,21 @@ const gql_query = `{ business(id: "yelp-san-francisco")
               } 
             } 
           }`;
+
 query(gql_query);
 */
-
 
 document.addEventListener("DOMContentLoaded", function() {
   if (document.querySelectorAll("#map").length > 0) {
     var js_file = document.createElement("script");
     js_file.type = "text/javascript";
     js_file.src =
-      "https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyBeAR2QSBsI-Xch0uTh1yOADUDHifjzWTY";
+      "https://maps.googleapis.com/maps/api/js?callback=app&key=AIzaSyBeAR2QSBsI-Xch0uTh1yOADUDHifjzWTY";
     document.getElementsByTagName("head")[0].appendChild(js_file);
   }
 });
 
-var map;
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -55,5 +40,74 @@ function initMap() {
     },
     zoom: 14
   });
+  
+  
+
 }
 window.initMap = initMap; //global scope
+
+/*
+LocationModel.Query = ko.observable("");
+
+LocationModel.searchResults = ko.computed(function() {
+  var q = LocationModel.Query().toLowerCase();
+  return LocationModel.locations.filter(function(i) {
+    return i.name.toLowerCase().indexOf(q) >= 0;
+  });
+});
+*/
+
+
+var Location = function(data) {
+  this.name = data.name;
+  this.lat = data.coordinates[0];
+  this.long = data.coordinates[1];
+  this.url = "";
+  this.pic_url = "";
+  this.address = data.address;
+  this.id = data.yelp_id;
+
+  this.visible = ko.observable(true);
+
+  // Yelp Graphql call
+
+  // Info Window
+
+  // Set Marker
+  this.marker = new google.maps.Marker({
+    position: new google.maps.LatLng(this.lat,this.long),
+    map: map,
+    title: data.name
+  });
+
+  this.marker.setMap(map);
+};
+
+var ViewModel = function() {
+   var _this = this;
+  this.locationsList = ko.observableArray([]);
+
+  LocationModel.locations.map(location => {
+    this.locationsList.push(new Location(location));
+  });
+
+  console.log(this.locationsList);
+
+  // initialize searchquery
+  this.Query = ko.observable("");
+// TODO: ES6 Arrow function
+  this.searchResults = ko.computed(function() {
+  return _this.locationsList().filter(function(location) {
+    return location.name.toLowerCase().indexOf(_this.Query().toLowerCase()) >= 0;
+  });
+});
+
+
+};
+
+
+function app(){
+  initMap();
+  ko.applyBindings(new ViewModel());
+}
+window.app = app;
